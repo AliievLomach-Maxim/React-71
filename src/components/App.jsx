9e2037cb2441b0ab7ba15b039e8bb0baf26280e7
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-05-03 21:34:45 +0300

    add less-04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import ToDoList from './ToDoList/ToDoList'
 import Modal from './Modal/Modal'
-import Counter from './Counter/Counter'
+// import Counter from './Counter/Counter'
 import Header from './Header/Header'
 import { Component } from 'react'
 import Parent from './Parent/Parent'
+import FormLogin from './FormLogin/FormLogin'
 
 class App extends Component {
 	state = { isShowModal: false }
@@ -14,28 +15,33 @@ class App extends Component {
 	closeModal = () => {
 		this.setState({ isShowModal: false })
 	}
-	HS = (e) => {
-		e.preventDefault()
 
-		console.log('e :>> ', e)
+	createUser = (data) => {
+		const newUser = {
+			...data,
+			id: Date.now(),
+			role: 'customer',
+		}
+		console.log(newUser)
 	}
 	render() {
 		return (
 			<div className='container'>
 				<Header open={this.openModal} />
-
-				<form action='' onSubmit={this.HS}>
-					<input type='text' name='qwe' />
-					<input type='text' name='qwe1' />
-					<input type='text' name='qwe2' />
-					<button>qwe</button>
-				</form>
 				{/* <Counter /> */}
 				<ToDoList />
 
-				<Parent close={this.closeModal} isOpen={this.state.isShowModal}>
+				{/* <Parent close={this.closeModal} isOpen={this.state.isShowModal}>
 					Some
-				</Parent>
+				</Parent> */}
+				{this.state.isShowModal && (
+					<Modal close={this.closeModal}>
+						<FormLogin
+							close={this.closeModal}
+							createUser={this.createUser}
+						/>
+					</Modal>
+				)}
 			</div>
 		)
 	}
