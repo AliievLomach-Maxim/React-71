5ae5e2a42382a08bb4a8383d446266ec0c9991b4
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-06-20 00:34:00 +0300

    add less

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,10 +2,12 @@ import { counterReducer } from './counter/counterReducer'
 import { productsReducer } from './products/products'
 import { todoReducer } from './todo/todoSlice'
 import { productsApi } from './productsApi'
+import { authReducer } from './auth/slice'
 
 export const reducer = {
 	todo: todoReducer,
 	counter: counterReducer,
 	products: productsReducer,
 	[productsApi.reducerPath]: productsApi.reducer,
+	auth: authReducer,
 }
